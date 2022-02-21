09b6b47a78f1e9685865bf17662debeaf4adcfcb
Rachel O'Grady <90624141+ogradyra@users.noreply.github.com>
2022-02-21 14:55:18 +0000

    Create ik_maths.h

---
+++ header_files/ik_maths.h
@@ -0,0 +1,12 @@
+#ifndef _IK_MATHS_H_
+#define _IK_MATHS_H_
+
+// GLM includes
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+glm::vec2 analytical_soln(glm::vec3 starting_pos);
+
+
+#endif
